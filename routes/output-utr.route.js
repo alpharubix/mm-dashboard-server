56a1657bdf65b042b411b53014cd5bbdc132354f
builtbysuraj <surajgupta3553@gmail.com>
2025-05-26 15:17:11 +0530

    refactor: Change file name of input utr controller

--- routes/output-utr.route.js
+++ routes/output-utr.route.js
@@ -1,9 +1,9 @@
 import express from 'express'
 
-import { outputUtrFtpController } from '../controllers/invoice-utr/fetch-from-ftp.js'
-import { getAllOutputUtrData } from '../controllers/invoice-utr/get-all-utr-data.js'
-import { getOutputUtrData } from '../controllers/invoice-utr/get-utr-data.js'
-import { outputUtrCsvParseAndSave } from '../controllers/invoice-utr/upload-csv-to-db-and-ftp.js'
+import { outputUtrFtpController } from '../controllers/invoice-utr/fetch-from-ftp.controller.js'
+import { getAllOutputUtrData } from '../controllers/invoice-utr/get-all-utr-data.controller.js'
+import { getOutputUtrData } from '../controllers/invoice-utr/get-utr-data.controller.js'
+import { outputUtrCsvParseAndSave } from '../controllers/invoice-utr/upload-csv-to-db-and-ftp.controller.js'
 import upload from '../middlewares/multer.js'
 
 const router = express.Router()
