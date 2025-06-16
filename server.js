04485c680117cde60010098635ae186e6250bd87
builtbysuraj <surajgupta3553@gmail.com>
2025-06-16 08:40:22 +0530

    Remove src folder

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 import { app } from './app.js'
 import { connectDB } from './db.js'
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 connectDB()
   .then(() => {
