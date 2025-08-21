46f1099e9e75e04e3397b29cf68bdcd80a8ec84f
builtbysuraj <surajgupta3553@gmail.com>
2025-08-21 11:45:12 +0530

    Add invoices.r1xchange.com to cors

--- utils/cors.js
+++ utils/cors.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 const allowedOrigins = [
   'https://mm-dashboard-client-three.vercel.app',
   'http://localhost:5173',
+  'https://invoices.r1xchange.com',
+  'invoices.r1xchange.com',
 ]
 
 const corsMiddleware = cors({
