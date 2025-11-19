c37def21315878a13230ff7a26de1f4f5efe616e
ashok <ashokmommyguy@gmail.com>
2025-11-19 14:13:56 +0530

    implemented available_limit check during creditlimit upload

--- models/distributor-list.model.js
+++ models/distributor-list.model.js
@@ -4,7 +4,7 @@ const allowedDistributor = new mongoose.Schema(
   {
     companyName: { type: String, required: true },
     distributorCode: { type: String, required: true },
-    distributorPhone: { type: String, required: true },
+    distributorPhone: { type: String, required: true }, //this field has to be unique
     distributorEmail: { type: String, required: true },
     lender: { type: String, required: true },
     anchorId: { type: String, required: true },
